bf0c00e98853f15d65654695060d15ddae00e619
22211tt4550 <22211tt4550@mail.tdc.edu.vn>
2024-10-16 08:23:53 +0700

    Thêm tính năng tự động đăng nhập cho a Thanh lười thêm.

--- GUI/frmLogin.cs
+++ GUI/frmLogin.cs
@@ -1,11 +1,14 @@
 ﻿using BUS.Danh_Muc;
 using DevExpress.XtraEditors;
 using DevExpress.XtraWaitForm;
+using DTO.Custom;
+using DTO.Utility;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +23,7 @@ namespace GUI
         public frmLogin()
         {
             InitializeComponent();
+
             this.KeyPreview = true; // Cho phép form nhận sự kiện phím
 
             this.KeyDown += LoginForm_KeyDown;
@@ -34,6 +38,36 @@ namespace GUI
                 StaffController objController = new StaffController();
                 objController.LoginProcess(txtTenDangNhap.Text, txtMatKhau.Text);
 
+                //B1. Kiểm tra xem file chứa thông tin đăng nhập có tồn tại trên máy không nếu không thì tạo mới
+                if (Directory.Exists(CConfig.CM_Cinema_FileManagement_Folder) == false)
+                    Directory.CreateDirectory(CConfig.CM_Cinema_FileManagement_Folder);
+
+                string strFileName = "userinfo.txt";
+                string strFullFilePath = CConfig.CM_Cinema_FileManagement_Folder + strFileName;
+
+                //Tạo file nếu file không tồn tại
+                if (File.Exists(strFullFilePath) == false)
+                {
+                    using (FileStream objFile = File.Create(strFullFilePath))
+                    {
+
+                    }
+                }
+
+                //Ghi các thông tin đã đc mã hóa vào file
+                using (Stream objStream = File.Open(strFullFilePath, FileMode.Open))
+                {
+                    using (StreamWriter objSW = new StreamWriter(objStream, Encoding.UTF8))
+                    {
+                        string strMaDangNhap_MD5 = CUtility.MD5_Encrypt(txtTenDangNhap.Text);
+                        string strMatKhau_MD5 = CUtility.MD5_Encrypt(txtMatKhau.Text);
+
+                        // Ghi từng dòng
+                        objSW.WriteLine(strMaDangNhap_MD5);
+                        objSW.WriteLine(strMatKhau_MD5);
+                    }
+                }
+
                 //Nếu pass hết thì đóng form
                 Close();
             }
@@ -45,23 +79,39 @@ namespace GUI
 
         private void btnThoat_Click(object sender, EventArgs e)
         {
-            if (DialogResult.Yes == MessageBox.Show("Bạn có muốn thoát chương trình?", "Thông báo",
-                MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            try
             {
-                StatusClose = true;
+                if (DialogResult.Yes == MessageBox.Show("Bạn có muốn thoát chương trình?", "Thông báo",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                {
+                    StatusClose = true;
 
-                // Thoát chương trình
-                Application.Exit();
+                    // Thoát chương trình
+                    Application.Exit();
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private void LoginForm_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            try
             {
-                btnDangNhap_Click(sender, e);
-                e.Handled = true; // Ngăn chặn sự kiện tiếp tục
+                if (e.KeyCode == Keys.Enter)
+                {
+                    btnDangNhap_Click(sender, e);
+                    e.Handled = true; // Ngăn chặn sự kiện tiếp tục
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private void LoginForm_Load(object sender, EventArgs e)
