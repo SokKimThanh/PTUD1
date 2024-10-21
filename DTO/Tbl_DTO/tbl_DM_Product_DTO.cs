1fd28c7cc96ca1b0cfe88ec90ec3158babf8d63c
Sok Kim Thanh <thanhsk1991@gmail.com>
2024-10-21 10:54:52 +0700

    Cập nhật UC sản phẩm

--- DTO/Tbl_DTO/tbl_DM_Product_DTO.cs
+++ DTO/Tbl_DTO/tbl_DM_Product_DTO.cs
@@ -11,6 +11,7 @@ namespace DTO.tbl_DTO
     /// </summary>
     public class tbl_DM_Product_DTO
     {
+        private long pD_AutoID;
         private string pD_NAME, pD_IMAGEURL;
         private float pD_QUANTITY, pD_PRICE;
 
@@ -18,17 +19,105 @@ namespace DTO.tbl_DTO
         {
         }
 
-        public tbl_DM_Product_DTO(string pD_NAME = null, string pD_IMAGEURL = null, float pD_QUANTITY = 0, float pD_PRICE = 0)
+        /// <summary>
+        /// constructor for combobox
+        /// </summary>
+        /// <param name="pD_AutoID"></param>
+        /// <param name="pD_NAME"></param>
+        public tbl_DM_Product_DTO(long pD_AutoID, string pD_NAME)
+        {
+            this.pD_AutoID = pD_AutoID;
+            this.pD_NAME = pD_NAME;
+        }
+
+        public tbl_DM_Product_DTO(string pD_NAME = null, string pD_IMAGEURL = null, float pD_QUANTITY = 0, float pD_PRICE = 0, long pD_AutoID = 0)
         {
             this.pD_NAME = pD_NAME;
             this.pD_IMAGEURL = pD_IMAGEURL;
             this.pD_QUANTITY = pD_QUANTITY;
             this.pD_PRICE = pD_PRICE;
+            this.pD_AutoID = pD_AutoID;
         }
 
-        public string PD_NAME { get => pD_NAME; set => pD_NAME = value; }
-        public string PD_IMAGEURL { get => pD_IMAGEURL; set => pD_IMAGEURL = value; }
-        public float PD_QUANTITY { get => pD_QUANTITY; set => pD_QUANTITY = value; }
-        public float PD_PRICE { get => pD_PRICE; set => pD_PRICE = value; }
+        public string PD_NAME
+        {
+            get => pD_NAME;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length <= 250)
+                {
+                    pD_NAME = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PD_NAME không được để trống và bắt buộc nhỏ hơn hoặc bằng 250 kí tự.");
+                }
+            }
+        }
+
+        public string PD_IMAGEURL
+        {
+            get => pD_IMAGEURL;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    pD_IMAGEURL = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PD_IMAGEURL không được để trống");
+                }
+            }
+        }
+
+        public float PD_QUANTITY
+        {
+            get => pD_QUANTITY;
+            set
+            {
+                if (value >= 0)
+                {
+                    pD_QUANTITY = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PD_QUANTITY bắt buộc lớn hơn hoặc bằng 0");
+                }
+            }
+        }
+
+        public float PD_PRICE
+        {
+            get => pD_PRICE;
+            set
+            {
+                if (value >= 0)
+                {
+                    pD_PRICE = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PD_PRICE must be greater than or equal to 0.");
+                }
+            }
+        }
+
+        public long PD_AutoID
+        {
+            get => pD_AutoID;
+            set
+            {
+                if (value > 0)
+                {
+                    pD_AutoID = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PD_AutoID must be greater than 0.");
+                }
+            }
+        }
+
     }
 }
--- GUI/UI/Modules/ucSanPham.cs
+++ GUI/UI/Modules/ucSanPham.cs
@@ -1,9 +1,11 @@
-﻿using DevExpress.XtraEditors;
+﻿using DevExpress.Utils;
+using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +23,8 @@ namespace GUI.UI.Modules
             this.layoutTitle.MinSize = new System.Drawing.Size(36, 36);
             // Ngăn không cho phép sửa dữ liệu trực tiếp trên GridView
             gridView1.OptionsBehavior.Editable = false;
-
+            // cho phép kéo thả hình vào ô hình
+            pictureBox.Properties.ShowZoomSubMenu = DefaultBoolean.True;
         }
 
         protected override void Load_Data()
@@ -33,6 +36,50 @@ namespace GUI.UI.Modules
         private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
 
+
+        }
+        /// <summary>
+        /// Nút thêm hình nếu người dùng thích bấm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Hiển thị hình ảnh trên PictureEdit
+                pictureBox.Image = Image.FromFile(openFileDialog.FileName);
+
+                // Lấy đường dẫn đầy đủ của file đã chọn
+                string selectedImagePath = openFileDialog.FileName;
+
+                // Hiển thị đường dẫn hình ảnh trong TextBox hoặc Label
+                txtUrlHinh.Text = selectedImagePath;
+            }
+        }
+        /// <summary>
+        /// Nhấp vào hình hiển thị dialog box chọn ảnh
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Hiển thị hình ảnh trên PictureEdit
+                pictureBox.Image = Image.FromFile(openFileDialog.FileName);
+
+                // Lấy đường dẫn đầy đủ của file đã chọn
+                string selectedImagePath = openFileDialog.FileName;
+
+                // Hiển thị đường dẫn hình ảnh trong TextBox hoặc Label
+                txtUrlHinh.Text = selectedImagePath;
+            }
         }
     }
 }
