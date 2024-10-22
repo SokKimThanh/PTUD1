7ffb58b713e934b4cc455d3a1cfb5968d4bec171
22211tt4550 <22211tt4550@mail.tdc.edu.vn>
2024-10-22 14:14:02 +0700

    Update

--- DAL/tbl_DM_Staff_DAL.cs
+++ DAL/tbl_DM_Staff_DAL.cs
@@ -8,20 +8,8 @@ namespace DAL
 {
     public class tbl_DM_Staff_DAL : BasicMethods<tbl_DM_Staff_DTO>
     {
-
         public override void AddData(tbl_DM_Staff_DTO obj)
         {
-            if (obj.ST_USERNAME.Trim() == "")
-                throw new Exception("Mã đăng nhập không được rỗng.");
-
-            if (obj.ST_PASSWORD.Trim() == "")
-                throw new Exception("Mật khẩu không được rỗng.");
-
-            //Kiểm tra xem mã đăng nhập có tồn tại
-            tbl_DM_Staff objCheck = DBDataContext.tbl_DM_Staffs.FirstOrDefault(it => it.ST_USERNAME.Trim() == obj.ST_USERNAME.Trim() && it.DELETED == 0);
-            if (objCheck != null)
-                throw new Exception("Mã đăng nhập đã tồn tại");
-
             tbl_DM_Staff objNew = new tbl_DM_Staff();
             CUtility.Clone_Entity(obj, objNew);
 
@@ -32,7 +20,7 @@ namespace DAL
         public override List<tbl_DM_Staff_DTO> GetList()
         {
             List<tbl_DM_Staff_DTO> arrRes = new List<tbl_DM_Staff_DTO>();
-            List<tbl_DM_Staff> arrDB = DBDataContext.tbl_DM_Staffs.Where(it => it.DELETED == 0).OrderByDescending(it => it.ST_AutoID).ToList();
+            List<tbl_DM_Staff> arrDB = DBDataContext.tbl_DM_Staffs.Where(it => it.DELETED != null || it.DELETED != 1).ToList();
 
             foreach (tbl_DM_Staff objDB in arrDB)
             {
@@ -60,50 +48,14 @@ namespace DAL
 
         public override void UpdateData(tbl_DM_Staff_DTO obj)
         {
-            if (obj.ST_USERNAME.Trim() == "")
-                throw new Exception("Mã đăng nhập không được rỗng.");
-
-            if (obj.ST_PASSWORD.Trim() == "")
-                throw new Exception("Mật khẩu không được rỗng.");
-
-            //Kiểm tra xem mã đăng nhập có tồn tại
-            tbl_DM_Staff objCheck = DBDataContext.tbl_DM_Staffs.FirstOrDefault(it => it.ST_AutoID != obj.ST_AutoID &&
-                                        it.ST_USERNAME.Trim() == obj.ST_USERNAME.Trim() && it.DELETED == 0);
-
-            if (objCheck != null)
-                throw new Exception("Mã đăng nhập đã tồn tại");
-
             tbl_DM_Staff objRes = DBDataContext.tbl_DM_Staffs.SingleOrDefault(it => it.ST_AutoID == obj.ST_AutoID);
 
             if (objRes != null)
             {
-                objRes.ST_USERNAME = obj.ST_USERNAME.Trim();
-                objRes.ST_PASSWORD = obj.ST_PASSWORD.Trim();
-                objRes.ST_NAME = obj.ST_NAME.Trim();
-                objRes.ST_PHONE = obj.ST_PHONE.Trim();
-                objRes.ST_CIC = obj.ST_CIC.Trim();
-                objRes.ST_NOTE = obj.ST_NOTE.Trim();
-                objRes.ST_LEVEL = obj.ST_LEVEL;
-                objRes.UPDATED = obj.UPDATED;
-                objRes.UPDATED_BY = obj.UPDATED_BY.Trim();
-                objRes.UPDATED_BY_FUNCTION = obj.UPDATED_BY_FUNCTION.Trim();
-
+                CUtility.Clone_Entity(obj, objRes);
                 DBDataContext.SubmitChanges();
             }
 
-        }
-
-        public tbl_DM_Staff_DTO GetDataByUserName(string strUserName)
-        {
-            tbl_DM_Staff objDB = DBDataContext.tbl_DM_Staffs.FirstOrDefault(it => it.ST_USERNAME.Trim() == strUserName.Trim() && it.DELETED == 0);
-            tbl_DM_Staff_DTO objRes = null;
-            if (objDB != null)
-            {
-                objRes = new tbl_DM_Staff_DTO();
-                CUtility.Clone_Entity(objDB, objRes);
-            }
-
-            return objRes;
         }
 
         public override void RemoveData(int id)
