9602cfc0ae8bcb44376e6ea937bedf9fa5a1c16e
Sok Kim Thanh <thanhsk1991@gmail.com>
2024-11-12 19:14:46 +0700

    Cập nhật DAL chi phí

--- BUS/Danh_Muc/tbl_SYS_Expense_BUS.cs
+++ BUS/Danh_Muc/tbl_SYS_Expense_BUS.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using DTO.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +13,51 @@ namespace BUS.Danh_Muc
     /// </summary>
     public class tbl_SYS_Expense_BUS
     {
+        private tbl_SYS_Expense_DAL data = new tbl_SYS_Expense_DAL();
+
+        /// <summary>
+        /// them 
+        /// </summary>
+        /// <param name="expense"></param>
+        /// <returns></returns>
+        public long Add(tbl_SYS_Expense expense)
+        {
+            return data.Add(expense);
+        }
+        /// <summary>
+        /// xoa
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Remove(long id)
+        {
+            return data.Remove(id);
+        }
+        /// <summary>
+        /// cap nhat
+        /// </summary>
+        /// <param name="expense"></param>
+        /// <returns></returns>
+        public bool Update(tbl_SYS_Expense expense)
+        {
+            return data.Update(expense);
+        }
+        /// <summary>
+        /// danh sach
+        /// </summary>
+        /// <returns></returns>
+        public List<tbl_SYS_Expense> GetAll()
+        {
+            return data.GetAll();
+        }
+        /// <summary>
+        /// tim 1 chi phi theo id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public tbl_SYS_Expense Find(long id)
+        {
+            return data.Find(id);
+        }
     }
 }
--- DAL/tbl_SYS_Expense_DAL.cs
+++ DAL/tbl_SYS_Expense_DAL.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DTO.Common;
+using DTO.Custom;
+using DTO.tbl_DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +14,182 @@ namespace DAL
     /// </summary>
     public class tbl_SYS_Expense_DAL
     {
+        private readonly string _connectionString = CConfig.CM_Cinema_DB_ConnectionString;
+
+        /// <summary>
+        /// them 
+        /// </summary>
+        /// <param name="expense"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public long Add(tbl_SYS_Expense expense)
+        {
+            try
+            {
+                using (var dbContext = new CM_Cinema_DBDataContext(_connectionString))
+                {
+                    var entity = new tbl_SYS_Expense
+                    {
+                        EX_EXTYPE_AutoID = expense.EX_EXTYPE_AutoID,
+                        EX_PRICE = expense.EX_PRICE,
+                        EX_QUANTITY = expense.EX_PRICE,
+                        EX_REASON = expense.EX_REASON,
+                        EX_STATUS = expense.EX_STATUS,
+
+                        DELETED = 0,
+                        CREATED = DateTime.Now,
+                        CREATED_BY = CCommon.MaDangNhap,
+                        CREATED_BY_FUNCTION = "Add",
+                        UPDATED = DateTime.Now,
+                        UPDATED_BY = CCommon.MaDangNhap,
+                        UPDATED_BY_FUNCTION = "Add"
+                    };
+                    dbContext.tbl_SYS_Expenses.InsertOnSubmit(entity);
+                    dbContext.SubmitChanges();
+
+                    return entity.EX_AutoID; // Trả về id vừa được thêm
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi thực thi thao tác với DB: {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// xoa
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public bool Remove(long id)
+        {
+            try
+            {
+                using (var dbContext = new CM_Cinema_DBDataContext(_connectionString))
+                {
+                    var entity = dbContext.tbl_SYS_Expenses.SingleOrDefault(t => t.EX_AutoID == id);
+
+                    if (entity != null)
+                    {
+                        entity.DELETED = 1;
+                        entity.UPDATED = DateTime.Now;
+                        entity.UPDATED_BY = CCommon.MaDangNhap;
+                        entity.UPDATED_BY_FUNCTION = "Remove";
+                        dbContext.SubmitChanges();
+                        return true; // Thao tác thành công
+                    }
+                    return false; // Không tìm thấy entity để xóa
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi thực thi thao tác với DB: {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// cap nhat
+        /// </summary>
+        /// <param name="expense"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public bool Update(tbl_SYS_Expense expense)
+        {
+            try
+            {
+                using (var dbContext = new CM_Cinema_DBDataContext(_connectionString))
+                {
+                    var entity = dbContext.tbl_SYS_Expenses.SingleOrDefault(t => t.EX_AutoID == expense.EX_AutoID);
+                    if (entity != null)
+                    {
+                        entity.EX_AutoID = expense.EX_AutoID;
+                        entity.EX_EXTYPE_AutoID = expense.EX_EXTYPE_AutoID;
+                        entity.EX_PRICE = expense.EX_PRICE;
+                        entity.EX_QUANTITY = expense.EX_PRICE;
+                        entity.EX_REASON = expense.EX_REASON;
+                        entity.EX_STATUS = expense.EX_STATUS;
+
+                        entity.UPDATED = DateTime.Now;
+                        entity.UPDATED_BY = CCommon.MaDangNhap;
+                        entity.UPDATED_BY_FUNCTION = "Update";
+                        dbContext.SubmitChanges();
+                        return true; // Thao tác thành công
+                    }
+                    return false; // Không tìm thấy entity để cập nhật
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi thực thi thao tác với DB: {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// danh sach
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public List<tbl_SYS_Expense> GetAll()
+        {
+            List<tbl_SYS_Expense> result = new List<tbl_SYS_Expense>();
+
+            try
+            {
+                using (var dbContext = new CM_Cinema_DBDataContext(_connectionString))
+                {
+                    var list = dbContext.tbl_SYS_Expenses.ToList();
+                    foreach (var item in list)
+                    {
+                        if (item.DELETED != 1)
+                        {
+                            tbl_SYS_Expense entity = new tbl_SYS_Expense()
+                            {
+                                EX_AutoID = item.EX_AutoID,
+                                EX_STATUS = item.EX_STATUS,
+                                EX_REASON = item.EX_REASON,
+                                EX_PRICE = item.EX_PRICE,
+                                EX_QUANTITY = item.EX_QUANTITY,
+                                EX_EXTYPE_AutoID = item.EX_EXTYPE_AutoID,
+                            };
+                            result.Add(entity);
+                        }
+                    }
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi thực thi thao tác với DB: {ex.Message}");
+            }
+        }
+        /// <summary>
+        /// tim 1 chi phi theo id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public tbl_SYS_Expense Find(long id)
+        {
+            try
+            {
+                using (var dbContext = new CM_Cinema_DBDataContext(_connectionString))
+                {
+                    return dbContext.tbl_SYS_Expenses
+                        .Where(t => t.EX_AutoID == id)
+                        .Select(item => new tbl_SYS_Expense
+                        {
+                            EX_AutoID = item.EX_AutoID,
+                            EX_STATUS = item.EX_STATUS,
+                            EX_REASON = item.EX_REASON,
+                            EX_PRICE = item.EX_PRICE,
+                            EX_QUANTITY = item.EX_QUANTITY,
+                            EX_EXTYPE_AutoID = item.EX_EXTYPE_AutoID,
+                        })
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi thực thi thao tác với DB: {ex.Message}");
+            }
+        }
     }
 }
