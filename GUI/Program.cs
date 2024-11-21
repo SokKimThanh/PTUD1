e988ad9a295a0956500d180ab8a8bd2ae31f9fc3
22211TT <TuanTM1804@outlook.com.vn>
2024-11-21 23:35:07 +0700

    Update connection

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using DTO.Custom;
+﻿using DTO.Common;
+using DTO.Custom;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -6,6 +7,7 @@ using System.Data.Sql;
 using System.Data.SqlClient;
 using System.IO;
 using System.Management;
+using System.Text;
 using System.Windows.Forms;
 using static DevExpress.XtraEditors.Mask.MaskSettings;
 
@@ -23,29 +25,31 @@ namespace GUI
             try
             {
                 v_iStep = 1;
-                Auto_Connect_DB_By_ManagementObjectSearcher();
+                Auto_Get_Connection_From_File();
 
                 v_iStep = 2;
                 if (CConfig.CM_Cinema_DB_ConnectionString == "")
-                    Auto_Connect_DB_By_SqlDataSourceEnumerator();
+                    Auto_Connect_DB_By_ManagementObjectSearcher();
 
                 v_iStep = 3;
                 if (CConfig.CM_Cinema_DB_ConnectionString == "")
-                    Auto_Connect_DB_Manual();
+                    Auto_Connect_DB_By_SqlDataSourceEnumerator();
 
                 v_iStep = 4;
                 if (CConfig.CM_Cinema_DB_ConnectionString == "")
-                    throw new Exception("Chuỗi kết nối không tồn tại. Đóng chương trình");
+                    Auto_Connect_DB_Manual();
 
                 v_iStep = 5;
-                CheckDatabaseExists();
+                if (CConfig.CM_Cinema_DB_ConnectionString == "")
+                    throw new Exception("Chuỗi kết nối không tồn tại. Đóng chương trình");
 
                 v_iStep = 6;
-                if (Directory.Exists(CConfig.CM_Cinema_FileManagement_Folder) == false)
-                    Directory.CreateDirectory(CConfig.CM_Cinema_FileManagement_Folder);
+                CheckDatabaseExists();
 
                 v_iStep = 7;
+                Save_Connection();
 
+                v_iStep = 8;
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new frmMain());
@@ -98,7 +102,7 @@ namespace GUI
                         catch (Exception)
                         {
                         }
-                    }                   
+                    }
                 }
 
                 v_objSearch.Dispose();
@@ -221,6 +225,98 @@ namespace GUI
         static void Config_SqlExpress()
         {
             CConfig.CM_Cinema_DB_ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=CM_Cinema_DB;Integrated Security=True;Encrypt=False";
+        }
+
+        static void Auto_Get_Connection_From_File()
+        {
+            try
+            {
+                if (Directory.Exists(CConfig.CM_Cinema_FileManagement_Folder) == false)
+                    Directory.CreateDirectory(CConfig.CM_Cinema_FileManagement_Folder);
+
+                if (Directory.Exists(CConfig.CM_Cinema_FileManagement_Folder + "\\ConnectionString\\") == false)
+                    Directory.CreateDirectory(CConfig.CM_Cinema_FileManagement_Folder + "\\ConnectionString\\");
+
+                string strFileName = "connectionString.txt";
+                string strFullFilePath = CConfig.CM_Cinema_FileManagement_Folder + "\\ConnectionString\\" + strFileName;
+
+                //Tạo file nếu file không tồn tại
+                if (File.Exists(strFullFilePath) == false)
+                {
+                    using (FileStream objFile = File.Create(strFullFilePath))
+                    {
+
+                    }
+                }
+
+                //Đọc thông tin file
+                using (Stream objStream = File.Open(strFullFilePath, FileMode.Open))
+                {
+                    using (StreamReader objSR = new StreamReader(objStream, Encoding.UTF8))
+                    {
+
+                        string v_strConnection = objSR.ReadLine();
+                        if (string.IsNullOrEmpty(v_strConnection) == false)
+                        {
+                            v_strConnection = v_strConnection.Trim();
+
+                            //Nếu không lấy đc chuỗi kết nối thì lấy từ file app config
+                            using (SqlConnection conn = new SqlConnection(v_strConnection))
+                            {
+                                conn.Open();
+                                conn.Close();
+
+                                CConfig.CM_Cinema_DB_ConnectionString = v_strConnection.Trim();
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        static void Save_Connection()
+        {
+            try
+            {
+                if (Directory.Exists(CConfig.CM_Cinema_FileManagement_Folder) == false)
+                    Directory.CreateDirectory(CConfig.CM_Cinema_FileManagement_Folder);
+
+                if (Directory.Exists(CConfig.CM_Cinema_FileManagement_Folder + "\\ConnectionString\\") == false)
+                    Directory.CreateDirectory(CConfig.CM_Cinema_FileManagement_Folder + "\\ConnectionString\\");
+
+                string strFileName = "connectionString.txt";
+                string strFullFilePath = CConfig.CM_Cinema_FileManagement_Folder + "\\ConnectionString\\" + strFileName;
+
+                //Tạo file nếu file không tồn tại
+                if (File.Exists(strFullFilePath) == false)
+                {
+                    using (FileStream objFile = File.Create(strFullFilePath))
+                    {
+
+                    }
+                }
+
+                //Lưu thông tin chuỗi kết nối
+                using (Stream objStream = File.Open(strFullFilePath, FileMode.Open))
+                {
+                    using (StreamWriter objSR = new StreamWriter(objStream, Encoding.UTF8))
+                    {
+                        objSR.WriteLine(CConfig.CM_Cinema_DB_ConnectionString);
+                    }
+                }
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
